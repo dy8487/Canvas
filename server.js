05f97c6caa9a1d41e69cfc54e6eaa1e059803fb3
dy.kwak <phnewma@hotmail.com>
2013-11-30 16:40:53 +0900

    socket.io 의 room 기능 사용한 채팅

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ server.listen(app.get('port'), function(){
 
 //canvas 관련
 io.sockets.on('connection', function (socket) {
-
+	
 	//added clients
 	socket.on("setClientId", function (data) {
 	    connectedClients[data.id] = { 
@@ -101,10 +101,70 @@ io.sockets.on('connection', function (socket) {
 	            isErase: data.isErase,
 	            id: data.id
 	        });
-	    });
+	});
+	    
 
 });
 
+
+var socketRoom = {};
+
+// socket.io 셋팅
+io.configure(function(){
+    io.set('transports', ['xhr-polling']);
+    io.set('polling duration', 10);
+    io.set('log level', 2);
+});
+
+io.sockets.on('connection', function(socket){
+    // 접속완료를 알림.
+    socket.emit('connected');
+    
+    // chat요청을 할 시
+    socket.on('requestRandomChat', function(data){
+        // 빈방이 있는지 확인
+        console.log('requestRandomChat');
+        var rooms = io.sockets.manager.rooms;
+        for (var key in rooms){
+            if (key == ''){
+                continue;
+            }
+            // 혼자있으면 입장
+            if (rooms[key].length == 1){
+                var roomKey = key.replace('/', '');
+                socket.join(roomKey);
+                io.sockets.in(roomKey).emit('completeMatch', {});
+                socketRoom[socket.id] = roomKey;
+                return;
+            }
+        }
+        // 빈방이 없으면 혼자 방만들고 기다림.
+        socket.join(socket.id);
+        socketRoom[socket.id] = socket.id;
+    });
+    
+    // 요청 취소 시
+    socket.on('cancelRequest', function(data){
+        socket.leave(socketRoom[socket.id]);
+    });
+    
+    // client -> server Message전송 시
+    socket.on('sendMessage', function(data){
+        console.log('sendMessage!');
+        io.sockets.in(socketRoom[socket.id]).emit('receiveMessage', data);
+    });
+    
+    // disconnect
+    socket.on('disconnect', function(data){
+        var key = socketRoom[socket.id];
+        socket.leave(key);
+        io.sockets.in(key).emit('disconnect');
+        var clients = io.sockets.clients(key);
+        for (var i = 0; i < clients.length; i++){
+            clients[i].leave(key);
+        }
+    });
+});
 
 
 
