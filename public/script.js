43d8d45bed01b747d1bcad3ed2e406d49cf602d3
dy8487 <phnewma@hotmail.com>
2013-12-07 11:09:40 +0900

    Update script.js
    사이즈 조정,한글화, scroll 수정

--- public/script.js
+++ public/script.js
@@ -25,119 +25,126 @@ var pseudo = "";
 
 // Init
 $(function() {
-        messageContainer = $('#messageInput');
-        submitButton = $("#submit");
-        bindButton();
-        window.setInterval(time, 1000*10);
-        $("#alertPseudo").hide();
-        //$('#modalPseudo').modal('show');
-        //$("#pseudoSubmit").click(function() {setPseudo()});
-        $("#chatEntries").slimScroll({height: '600px'});
-        submitButton.click(function() {sentMessage();});
-        setHeight();
-        
-        // 추가. 정성일
-        $("#messageInput").keydown(function(e){
-	        if(e.keyCode == 13){
-	            sentMessage();
-	       }
-	    });
+	messageContainer = $('#messageInput');
+	submitButton = $("#submit");
+	bindButton();
+	window.setInterval(time, 1000 * 10);
+	$("#alertPseudo").hide();
+	//$('#modalPseudo').modal('show');
+	//$("#pseudoSubmit").click(function() {setPseudo()});
+	$("#chatEntries").slimScroll({
+		height : '300px'
+	});
+	submitButton.click(function() {
+		sentMessage();
+	});
+	setHeight();
+
+	// 추가. 정성일
+	$("#messageInput").keydown(function(e) {
+		if (e.keyCode == 13) {
+			sentMessage();
+			//setScroll();
+		}
+	});
 });
 
 //Socket.io
 var socket = io.connect();
 socket.on('connect', function() {
-        console.log('connected');
+	console.log('connected');
 });
 socket.on('nbUsers', function(msg) {
-        $("#nbUsers").html(msg.nb);
+	$("#nbUsers").html(msg.nb);
 });
 socket.on('message', function(data) {
-        addMessage(data['message'], data['pseudo'], new Date().toISOString(), false);
-        console.log(data);
+	addMessage(data['message'], data['pseudo'], new Date().toISOString(), false);
+	console.log(data);
 });
 
 //Help functions
 function sentMessage() {
-        if (messageContainer.val() != "") 
-        {
-                if (pseudo == "") 
-                {
-                        $('#modalPseudo').modal('show');
-                }
-                else 
-                {
-                        socket.emit('message', messageContainer.val());
-                        addMessage(messageContainer.val(), "Me", new Date().toISOString(), true);
-                        messageContainer.val('');
-                        submitButton.button('loading');
-                }
-        }
+	if (messageContainer.val() != "") {
+		if (pseudo == "") {
+			$('#modalPseudo').modal('show');
+		} else {
+			socket.emit('message', messageContainer.val());
+			addMessage(messageContainer.val(), "Me", new Date().toISOString(), true);
+			messageContainer.val('');
+			submitButton.button('loading');
+		}
+	}
 }
+
 function addMessage(msg, pseudo, date, self) {
-        if(self) var classDiv = "row message self";
-        else var classDiv = "row message";
-        $("#chatEntries").append('<div class="'+classDiv+'"><p class="infos"><span class="pseudo">'+pseudo+'</span>, <time class="date" title="'+date+'">'+date+'</time></p><p>' + msg + '</p></div>');
-        time();
+	if (self)
+		var classDiv = "row message self";
+	else
+		var classDiv = "row message";
+	$("#chatEntries").append('<div class="' + classDiv + '"><p class="infos"><span class="pseudo">' + pseudo + '</span>, <time class="date" title="' + date + '">' + date + '</time></p><p>' + msg + '</p></div>');
+	
+	// Scroll 맞춤. 정성일
+	var divScroll = document.getElementById('chatEntries');
+	var scrollHeight = divScroll.scrollHeight;
+	divScroll.scrollTop = scrollHeight;
+	
+	var slimScroll = $(".slimScrollRail").siblings().css("top", scrollHeight+"px");
+			
+	time();
 }
 
 function bindButton() {
-        submitButton.button('loading');
-        messageContainer.on('input', function() {
-                if (messageContainer.val() == "") submitButton.button('loading');
-                else submitButton.button('reset');
-        });
+	submitButton.button('loading');
+	messageContainer.on('input', function() {
+		if (messageContainer.val() == "")
+			submitButton.button('loading');
+		else
+			submitButton.button('reset');
+	});
 }
+
 /*
-function setPseudo() {
-        if ($("#pseudoInput").val() != "")
-        {
-                socket.emit('setPseudo', $("#pseudoInput").val());
-                socket.on('pseudoStatus', function(data){
-                        if(data == "ok")
-                        {
-                                $('#modalPseudo').modal('hide');
-                                $("#alertPseudo").hide();
-                                pseudo = $("#pseudoInput").val();                                
-                        }
-                        else
-                        {
-                                $("#alertPseudo").slideDown();
-                        }
-                });
-        }
-}*/
+ function setPseudo() {
+ if ($("#pseudoInput").val() != "")
+ {
+ socket.emit('setPseudo', $("#pseudoInput").val());
+ socket.on('pseudoStatus', function(data){
+ if(data == "ok")
+ {
+ $('#modalPseudo').modal('hide');
+ $("#alertPseudo").hide();
+ pseudo = $("#pseudoInput").val();
+ }
+ else
+ {
+ $("#alertPseudo").slideDown();
+ }
+ });
+ }
+ }*/
 
 function setPseudo() {
-	
-		console.log("#### TEST - setPseudo() ");
-		
-        if ($(".userNameInput").val() != "")
-        {
-                socket.emit('setPseudo', $(".userNameInput").val());
-                socket.on('pseudoStatus', function(data){
-                        if(data == "ok")
-                        {
-                                $('#modalPseudo').modal('hide');
-                                $("#alertPseudo").hide();
-                                pseudo = $(".userNameInput").val(); 
-                                console.log("#### TEST - Ok : " + pseudo);                               
-                        }
-                        else
-                        {
-                                $("#alertPseudo").slideDown();
-                                console.log("#### TEST - Error ");
-                        }
-                });
-        }
+	if ($(".userNameInput").val() != "") {
+		socket.emit('setPseudo', $(".userNameInput").val());
+		socket.on('pseudoStatus', function(data) {
+			if (data == "ok") {
+				$('#modalPseudo').modal('hide');
+				$("#alertPseudo").hide();
+				pseudo = $(".userNameInput").val();
+			} else {
+				$("#alertPseudo").slideDown();
+			}
+		});
+	}
 }
 
 function time() {
-        $("time").each(function(){
-                $(this).text($.timeago($(this).attr('title')));
-        });
+	$("time").each(function() {
+		$(this).text($.timeago($(this).attr('title')));
+	});
 }
+
 function setHeight() {
-        $(".slimScrollDiv").height('603');
-        $(".slimScrollDiv").css('overflow', 'visible');
+	$(".slimScrollDiv").height('303');
+	$(".slimScrollDiv").css('overflow', 'visible');
 }
